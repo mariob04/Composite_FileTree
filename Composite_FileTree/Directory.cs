48140d3ff87684e643b1e0de3eabe41a4e198ee6
Mario Bilic <mario.bilic4@gmail.com>
2020-06-09 16:17:12 +0200

    add Directory, and mock up directory content

---
+++ Composite_FileTree/Directory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Composite_FileTree
+{
+    class Directory : FileSystemItem
+    {
+        public List<FileSystemItem> items { get; } = new List<FileSystemItem>();
+
+        public Directory(string name) : base(name)
+        {
+
+        }
+        public override decimal GetSizeinKB()
+        {
+            try
+            {
+                return this.items.Sum(x => x.GetSizeinKB());
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public void Add(FileSystemItem newItem)
+        {
+            this.items.Add(newItem);
+        }
+        public void Remove(FileSystemItem deleteItem)
+        {
+            this.items.Remove(deleteItem);
+        }
+    }
+}
